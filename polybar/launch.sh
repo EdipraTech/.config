7c9bc168cbd6e5d87f183548b79c246c327f0397
Daniel Arpide <edipra.tech@gmail.com>
2023-11-13 09:14:22 -0500

    polybar fix

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,6 +4,6 @@
 # If all your bars have ipc enabled, you can use 
 # polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-killall -q polybar
+pkill polybar
 
 polybar --reload example &
